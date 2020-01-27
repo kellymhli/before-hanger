f896bb89ee3fc7e2454919936d0d271a1be9d5e9
Kelly <55807683+kellymhli@users.noreply.github.com>
2020-01-27 12:25:14 -0800

    Pass client variables from env to app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 import Title from './components/Title';
 import Form from './components/Form';
 import Restaurant from './components/Restaurant';
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const API_KEY = process.env.REACT_APP_API_KEY;
+console.log(CLIENT_ID)
 
 class App extends React.Component {
+
   getOptions = (e) => {
     e.preventDefault();
     const values = e.target;
