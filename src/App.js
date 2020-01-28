a048267938598ab4767bc7505a28dfedb3f2b7cc
Kelly <55807683+kellymhli@users.noreply.github.com>
2020-01-27 18:18:00 -0800

    Add state, endpoint, and header for yelp api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 import Title from './components/Title';
 import Form from './components/Form';
 import Restaurant from './components/Restaurant';
-const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+
 const API_KEY = process.env.REACT_APP_API_KEY;
+const ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
+const HEADERS: {'Authorization': `bearer ${API_KEY}`}
 
 class App extends React.Component {
 
-  getOptions = (e) => {
+  state = {
+    'term': undefined,
+    'location': undefined,
+    'latitude': undefined,
+    'longitude': undefined,
+    'radius': 10000,
+    'categories': undefined,
+    'locale': 'en_US',
+    'limit': 50,
+    'price': undefined,
+    'open_now': true,
+  }
+
+  getOptions = async (e) => {
     e.preventDefault();
-    const values = e.target;
+    const values = e.target.elements;
     console.log(values);
+    const api_call = await fetch(`${ENDPOINT}`);
+    console.log(api_call);
   }
 
   render() {
