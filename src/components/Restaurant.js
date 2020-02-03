5a08e15ea76c56e5aacf9bf7aa128030d3a67c1a
Kelly <55807683+kellymhli@users.noreply.github.com>
2020-02-03 12:34:51 -0800

    Put restaurant details in bootstrap columns

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -3,18 +3,21 @@ import React from 'react';
 const Restaurant = props => {
     return (
         <div className="restaurant-container">
-            <h3>Done. You're going to: <br /><br />{props.name}</h3>
-            <div>
-                <img src={props.imgUrl} alt="{props.name}" width="500" height="350" />
-                <br /><a href={props.url} target="_blank">Yelp Link</a>
-                <br />Reviews: {props.reviewCount}
-                <br />Price: {props.price}
-                <br />Rating: {props.rating}
-                <br />{props.lat}
-                <br />{props.long}
-                <br />Call: {props.phone}
-                <br />{props.displayAddress[0]}
-                <br />{props.displayAddress[1]}
+            <br /><h3>Done. You're going to: <br />{props.name}</h3>
+            <div class="row">
+                <div class="md-col-4 res-details">
+                    <img src={props.imgUrl} alt="{props.name}" width="300" height="200" />
+                </div>
+                <div class="md-col-8 res-details">
+                    <a href={props.url} target="_blank">Yelp Link</a>
+                    <br />Reviews: {props.reviewCount}
+                    <br />Price: {props.price}
+                    <br />Rating: {props.rating}
+                    <br />{props.lat}, {props.long}
+                    <br />Call: {props.phone}
+                    <br />{props.displayAddress[0]}
+                    <br />{props.displayAddress[1]}
+                </div>
             </div>
         </div>
     );
